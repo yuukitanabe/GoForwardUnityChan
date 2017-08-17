b127507af87269118832d22c9369ca7f54c26e27
田辺祐基 <dqalg253@ybb.ne.jp>
2017-08-17 20:05:37 +0900

    Lesson7  課題提出
    キューブと地面、キューブ同士の接触時の効果音　動作確認しました。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+
+    private float speed = -0.2f;
+
+    private float deadLine = -10;
+
+
+    private float groundLevel = -0.3f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(this.speed, 0, 0);
+
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+
+       
+	}
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+
+        if (other.gameObject.tag == "groundTag" || other.gameObject.tag == "CubePrefabTag")
+        { 
+
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+}
